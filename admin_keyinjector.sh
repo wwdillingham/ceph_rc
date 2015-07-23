131015844797647e0c34d26a041666c6b8e8f914
Wes <wes_dillingham@harvard.edu>
2015-07-23 14:50:12 -0400

    debugging output

--- admin_keyinjector.sh
+++ admin_keyinjector.sh
@@ -2,14 +2,20 @@
 set -e
 
 : ${CEPH_ADMIN_USER:?"Need to set CEPH_ADMIN_USER environment variable"}
-
+echo "CEPH_ADMIN_USER is $CEPH_ADMIN_USER" >> /tmp/keyscript.log
 #create ceph admin user (root user for ceph manipulation) - comes from an evn variable CEPH_ADMIN_USER (in OpenNebula provided by conttextualization custom_vars)
 useradd -d /home/$CEPH_ADMIN_USER -m $CEPH_ADMIN_USER
+echo "past useradd" >> /tmp/keyscript.log
 echo "$CEPH_ADMIN_USER ALL = (root) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$CEPH_ADMIN_USER
+echo "past sudo" >> /tmp/keyscript.log
 chmod 0440 /etc/sudoers.d/$CEPH_ADMIN_USER
+echo "past chmod" >> /tmp/keyscript.log
 
 IDRSAPRIVATE=`echo /mnt/idrsaprivate`
 IDRSAPUBLIC=`echo /mnt/idrsapublic`
+
+echo "IDRSAPRIVATE is $IDRSAPRIVATE" >> /tmp/keyscript.log
+echo "IDRSAPUBLIC is $IDRSAPUBLIC" >> /tmp/keyscript.log
 
 echo "$IDRSAPRIVATE" > /tmp/idpriv
 echo "$IDRSAPUBLIC" > /tmp/idpub
