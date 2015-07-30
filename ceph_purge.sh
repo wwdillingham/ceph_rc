5fc0327d195762a6e49107ad0d607bf7740de234
Wes <wes_dillingham@harvard.edu>
2015-07-30 16:03:34 -0400

    remove keyring files in purge script

--- ceph_purge.sh
+++ ceph_purge.sh
@@ -1,3 +1,4 @@
 ceph-deploy purge $_ADMIN1 $_MON0 $_MON1 $_MON2 $_MDS0 $_OSD0 $_OSD1 $_OSD2
 ceph-deploy purgedata $_ADMIN1 $_MON0 $_MON1 $_MON2 $_MDS0 $_OSD0 $_OSD1 $_OSD2
 ceph-deploy forgetkeys
+rm /tmp/userdata_launchpad/*.keyring
