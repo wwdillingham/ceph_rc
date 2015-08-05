d5b7f7df92498885c80aac8301e14a620c36a6b6
Wes <wes_dillingham@harvard.edu>
2015-08-05 16:42:56 -0400

    end of day progress with templatizing script

--- puppet-apply-test.pp
+++ puppet-apply-test.pp
@@ -1,6 +1,6 @@
 $mons = ['mon01', 'mon02', 'mon03']
 $mdss = ['mds01']
-$osds = ['osd01', 'osd02', 'osd03']
+$osdss = ['osd01', 'osd02', 'osd03']
 file { '/tmp/mytestfile.txt':
   ensure => present,
   content => template('/Users/wesd/repos/ceph_rc/create_ceph_cluster.sh.erb')
