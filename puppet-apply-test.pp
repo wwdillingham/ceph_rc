57020a4235219c23418ccd3e7df9d85480a28808
Wes <wes_dillingham@harvard.edu>
2015-08-06 15:22:17 -0400

    puppet-apply file now matches for rapid testing

--- puppet-apply-test.pp
+++ puppet-apply-test.pp
@@ -1,6 +1,6 @@
-$mons = ['mon01.rc.fas.harvard.edu', 'mon02.rc.fas.harvard.edu', 'mon03.rc.fas.harvard.edu']
-$mdss = ['mds01.rc.fas.harvard.edu']
-$osds = ['osd01.rc.fas.harvard.edu', 'osd02.rc.fas.harvard.edu', 'osd03.rc.fas.harvard.edu']
+$mon_hosts = ['mon01.rc.fas.harvard.edu', 'mon02.rc.fas.harvard.edu', 'mon03.rc.fas.harvard.edu']
+$mds_hosts = ['mds01.rc.fas.harvard.edu']
+$osd_hosts = ['osd01.rc.fas.harvard.edu', 'osd02.rc.fas.harvard.edu', 'osd03.rc.fas.harvard.edu']
 $numreplicas = "3"
 $pools = ['poola', 'poolb']
 file { '/tmp/mytestfile.txt':
