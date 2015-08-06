0337f3851410da4869418723e294c8cdaf397290
Wes <wes_dillingham@harvard.edu>
2015-08-06 14:56:02 -0400

    variable names more appropriate and now matching puppet heira

--- puppet-apply-test.pp
+++ puppet-apply-test.pp
@@ -2,7 +2,7 @@ $mons = ['mon01.rc.fas.harvard.edu', 'mon02.rc.fas.harvard.edu', 'mon03.rc.fas.h
 $mdss = ['mds01.rc.fas.harvard.edu']
 $osds = ['osd01.rc.fas.harvard.edu', 'osd02.rc.fas.harvard.edu', 'osd03.rc.fas.harvard.edu']
 $numreplicas = "3"
-$pools = ['poola']
+$pools = ['poola', 'poolb']
 file { '/tmp/mytestfile.txt':
   ensure => present,
   content => template('/Users/wesd/repos/ceph_rc/create_ceph_cluster.sh.erb')
