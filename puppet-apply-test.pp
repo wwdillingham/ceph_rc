15c29260886cd521e213fec54b386fb10681e4e0
Wes <wes_dillingham@harvard.edu>
2015-08-06 16:51:04 -0400

    now handling hypens in hostnames

--- puppet-apply-test.pp
+++ puppet-apply-test.pp
@@ -1,6 +1,6 @@
 $mon_hosts = ['mon01.rc.fas.harvard.edu', 'mon02.rc.fas.harvard.edu', 'mon03.rc.fas.harvard.edu']
 $mds_hosts = ['mds01.rc.fas.harvard.edu']
-$osd_hosts = ['osd01.rc.fas.harvard.edu', 'osd02.rc.fas.harvard.edu', 'osd03.rc.fas.harvard.edu']
+$osd_hosts = ['osd01-afterhyphen.rc.fas.harvard.edu', 'osd02-afterhyphen.rc.fas.harvard.edu', 'osd03-afterhyphen.rc.fas.harvard.edu']
 $numreplicas = "3"
 $pools = ['poola', 'poolb']
 file { '/tmp/mytestfile.txt':
