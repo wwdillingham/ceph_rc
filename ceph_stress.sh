6b9e7133c4b174093fc3384a66917b06bf2694f5
Wes <wes_dillingham@harvard.edu>
2015-11-09 14:26:27 -0500

    input checking for rbd_bonnie option

--- ceph_stress.sh
+++ ceph_stress.sh
@@ -38,8 +38,10 @@ function print_help() {
         echo "--replication_size=integer number of replicas to make [required for non-existent pools, ignored for existing pools]"
         echo "--pg_num=integer number of placement groups for the pool [required for non-existent pools, ignored for existing pools]"
         echo "----------------------------------------------"
-	      echo "--rbd_bonnie options:"
-        echo "--bonnie-string=\"string\" enclose bonnie++ options to run in parentheses [required]"
+        echo "--rbd_bonnie options:"
+        echo "--bonnie_string=\"string\" enclose bonnie++ options to run in parentheses [required]"
+        echo "--num_block_devices=integer [required]"
+        echo "--block_device_size=integer size in MB [required]"
         echo "--pool=string pool name to use, will create if non-existent but will not remove pool [required]"
         echo "--replication_size  number of replicas to make [required for non-existent pools, ignored for existing pools]"
         echo "--pg_num=integer number of placement groups for the pool [required for non-existent pools, ignored for existing pools]"
@@ -119,7 +121,7 @@ do
     TEST_TIME_IN_SEC=$VALUE
   elif [[ $KEY == "--pool" ]]
     POOL_NAME=$VALUE
-  elif [[ $KEY == "--replication_size"]]
+  elif [[ $KEY == "--replication_size" ]]
     REPLICATION_SIZE=$VALUE
   elif [[ $KEY == "--pg_num" ]]
     PG_NUM=$VALUE
@@ -127,9 +129,9 @@ do
 done
 
 if ! [[ $NUM_BLOCK_DEVICE =~ ^[1-9]+$ ]]; then
-            echo "ERROR: --num_block_devices must be a positive integer"
-            print_help
-            exit
+  echo "ERROR: --num_block_devices must be a positive integer"
+  print_help
+  exit
 fi
 if ! [[ $SIZE_BLOCK_DEVICE =~ ^[1-9]+$ ]]; then
   echo "ERROR: --block_device_size must be a positive integer"
@@ -170,6 +172,57 @@ fi
 
 
 function check_input_args_for_rbd_bonnie() {
+  #At this point the first arg has been stripped off of the parameter list.
+  for arg in "$@"
+  do
+  	KEY=`echo $arg | awk -F "=" '{print $1}'`
+    VALUE=`echo $arg | awk -F "=" '{print $2}'`
+
+  	#Check if there is a flag that shouldnt be there
+if [[ $KEY != "--bonnie_string" && $KEY != "--pool" && $KEY != "--replication_size" && $KEY != "--pg_num" && $KEY != "--num_block_devices" && $KEY != "--block_device_size" ]]; then
+  echo "ERROR: $arg is not a valid parameter"
+  print_help
+  exit
+fi
+
+  if [[ $KEY == "--num_block_devices" ]]; then
+    NUM_BLOCK_DEVICE=$VALUE
+  elif [[ $KEY == "--block_device_size" ]]
+    SIZE_BLOCK_DEVICE=$VALUE
+  elif [[ $KEY == "--pool" ]]
+    POOL_NAME=$VALUE
+  elif [[ $KEY == "--replication_size" ]]
+    REPLICATION_SIZE=$VALUE
+  elif [[ $KEY == "--pg_num" ]]
+    PG_NUM=$VALUE
+  elif [[ $key == "--bonnie_string" ]]
+    BONNIE_STRING=$VALUE
+  fi
+
+done  
+  
+if ! [[ $NUM_BLOCK_DEVICE =~ ^[1-9]+$ ]]; then
+  echo "ERROR: --num_block_devices must be a positive integer"
+  print_help
+  exit
+fi
+if ! [[ $SIZE_BLOCK_DEVICE =~ ^[1-9]+$ ]]; then
+  echo "ERROR: --block_device_size must be a positive integer"
+  print_help
+  exit
+fi
+if ! [[ $REPLICATION_SIZE =~ ^[2-9]+$ ]]; then
+  echo "ERROR: --replication_size must be a positive integer greater than 2"
+  print_help
+  exit
+fi
+
+if ! [[ $POOL_NAME =~ ^[0-9A-Za-z_]+$ ]]; then
+  echo  "ERROR: --pool must be alphanumberic, including underscores, no spaces"
+  print_help
+  exit
+fi
+  
 }
 
 function check_input_args_for_rados_bench() {
@@ -189,7 +242,7 @@ echo "dollar sign 2: $2"
 
 if [[ $1 == "--help" ]] 
 	print_help
-elif [[  $1 -ne "--rbd_dd" && $1 -ne "--rados_bench" && $1 -ne "--rbd_benchwrite" &&  $1 -ne "rbn_bonnie"]]; then
+elif [[  $1 -ne "--rbd_dd" && $1 -ne "--rados_bench" && $1 -ne "--rbd_benchwrite" &&  $1 -ne "rbd_bonnie" ]]; then
 	echo "ERROR: Invalid first option"
         print_help
 elif [[ $1 == "--rbd_dd" ]]
@@ -197,17 +250,26 @@ elif [[ $1 == "--rbd_dd" ]]
 	if [[ $# -eq 6 || $# -eq 8 ]]; then #the correct number of args
 	  check_input_args_for_rbd_dd $2 $3 $4 $5 $6 $7 $8 
 	else
-	  echo "Wrong number of arguments received for rbd_dd"v
+	  echo "Wrong number of arguments received for rbd_dd"
 	  print_help
 	  exit
 	fi 
 	rbd_dd
+elif [[ $1 == "--rbd_bonnie" ]]
+	echo "Performing rbd mounts and doing a bonnie run upon that rbd mount"
+  if [[ $# -eq 5 || $# -eq 7 ]]; then
+    check_input_args_for_rbd_bonnie $2 $3 $4 $5 $6 $7
+  else
+    echo "Wrong number of arguments received for --rbd_bonnie"
+    print_help
+    exit
+  fi 
 elif [[ $1 == "-rados_bench" ]]
 	echo "Engaging rados bench utility"
-	rados_bench()
+	rados_bench
 elif [[ $1 == "--rbd_benchwrite" ]]
 	echo "Engaging rbd bench-write system"
-	rbd_benchwrite()
+	rbd_benchwrite
 else
 	echo "ERROR: input is malformed"
 	print_help
